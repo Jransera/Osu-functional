3c61df499a690278803a411fd57cf4195eece0be
madeleinemin8 <56084755+madeleinemin8@users.noreply.github.com>
2021-03-15 13:21:14 -0500

    Contains dots

--- Osu.elm
+++ Osu.elm
@@ -151,8 +151,8 @@ subscriptions _ model =
 pointGenerator : Generator Point
 pointGenerator =
   let
-    x = ( Random.float 100 1100) 
-    y = ( Random.float 100 600)
+    x = ( Random.float 100 1000) 
+    y = ( Random.float 100 450)
   in
     Random.map2 Point x y 
 
